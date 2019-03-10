c4155ded6bf3cb15197fcf1c4c928a3a54150688
raybagas <raymy.bagas@gmail.com>
2019-03-11 04:04:38 +0700

    initial commitment

--- app/src/main/java/com/ray/chat/MainActivity.java
+++ app/src/main/java/com/ray/chat/MainActivity.java
@@ -1,14 +1,27 @@
 package com.ray.chat;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
-
+    Button register;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    register=findViewById(R.id.button2);
+    register.setOnClickListener(new View.OnClickListener() {
+        @Override
+        public void onClick(View v) {
+            startActivity(new Intent(MainActivity.this,Register.class));
+        }
+    });
+
+        Toast.makeText(this, "Ray Samudra", Toast.LENGTH_SHORT).show();
     }
 }
 /*hihihihih*/
--- app/src/main/java/com/ray/chat/Register.java
+++ app/src/main/java/com/ray/chat/Register.java
@@ -1,13 +1,76 @@
 package com.ray.chat;
 
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.FirebaseFirestore;
+
+import java.util.HashMap;
+import java.util.Map;
 
 public class Register extends AppCompatActivity {
 
+    EditText username, password, rpassword, phone, email;
+    Button regis;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
+
+        // Access a Cloud Firestore instance from your Activity
+        final FirebaseFirestore db = FirebaseFirestore.getInstance();
+        username=findViewById(R.id.name);
+        password=findViewById(R.id.pass);
+        rpassword=findViewById(R.id.repass);
+        phone=findViewById(R.id.phone);
+        email=findViewById(R.id.email);
+        regis=findViewById(R.id.registra);
+        // Create a new user with a first and last name
+        regis.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (password.getText().toString().equals(rpassword.getText().toString())
+                        && !username.getText().toString().isEmpty()
+                        && !password.getText().toString().isEmpty()
+                        && !rpassword.getText().toString().isEmpty()
+                        && !phone.getText().toString().isEmpty()
+                        && !email.getText().toString().isEmpty()){
+                    Map<String, Object> user;
+                    user = new HashMap<>();
+                    user.put("name", username.getText().toString());
+                    user.put("pass", password.getText().toString());
+                    user.put("phone", phone.getText().toString());
+                    user.put("email", email.getText().toString());
+                    // Add a new document with a generated ID
+                    db.collection("users")
+                            .add(user)
+                            .addOnSuccessListener(new OnSuccessListener<DocumentReference>() {
+                                @Override
+                                public void onSuccess(DocumentReference documentReference) {
+                                    Toast.makeText(Register.this, "Success", Toast.LENGTH_SHORT).show();
+                                }
+                            })
+                            .addOnFailureListener(new OnFailureListener() {
+                                @Override
+                                public void onFailure(@NonNull Exception e) {
+                                    Toast.makeText(Register.this, "Fail", Toast.LENGTH_SHORT).show();
+                                }
+                            });
+                } else {
+                    Toast.makeText(Register.this, "Password gak sama", Toast.LENGTH_SHORT).show();
+                }
+
+            }
+        });
+
     }
 }
